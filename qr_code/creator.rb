ae1416eaee27b29a3e4b533bdd709d85495859c6
Danilo Barion Nogueira <danilo.barion@gmail.com>
2019-06-09 13:22:20 -0300

    Reorganize QR Code files

---
+++ qr_code/creator.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rqrcode'
+require 'erb'
+require 'launchy'
+
+class QrCode::Creator
+  QRCODE_FILENAME = 'qr_code.html'
+
+  def self.create(uuid)
+    @qr = RQRCode::QRCode.new(uuid)
+
+    template = File.open('template.erb', 'rb', &:read)
+    result = ERB.new(template).result(binding)
+
+    File.open(QRCODE_FILENAME, 'w') { |file| file.write(result) }
+    Launchy.open("file:///home/danilo/oss/nubank_rb/#{QRCODE_FILENAME}")
+  end
+end
--- renderer.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'erb'
-
-template = File.open('qr_code_template.erb', 'rb', &:read)
-result = ERB.new(template).result
-
-File.open('qr_code_result.html', 'w') { |file| file.write(result) }
